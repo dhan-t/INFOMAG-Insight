9040344a0ff89aa13a7a81af5a916eb5a63b38cd
Jasper Maraño <57208299+cleruu@users.noreply.github.com>
2024-03-03 17:01:38 +0800

    a
    a

---
+++ Versions/v.2.0.1/insight_flatlaf_revamp/src/main/java/colorSchemes/colorCodes.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package colorSchemes;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author Dhan Michea
+ */
+public class colorCodes {
+    
+    public static final Color mainAccent = new Color(22,255,255)
+;
+    public static final Color darkmode_elevatedColor_grey = Color.WHITE;
+    public static final Color darkmode_White = Color.BLACK;
+    public static final Color darkmode_Gray = new Color(0, 128, 255);
+   
+    
+    
+}
